3c5cd92402190b8c46d25438ce678fb191ae4539
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-06-24 14:43:09 -0300

    updated

---
+++ TrotsSistemas/src/main/java/control/RelatorioManager.java
@@ -0,0 +1,21 @@
+package control;
+
+import java.util.List;
+
+/**
+ *
+ * @author LUIS DAS ARTIMANHAS
+ */
+public class RelatorioManager {
+
+    public RelatorioManager() {
+    }
+
+    public void RelatorioComConexao() {
+
+    }
+    
+    public void RelatorioComLista(List lista,String nome) {
+
+    }
+}
